dab03284a55e17213acfa1cbc87dc22cd5758c70
Scorpio28-Oficial <77907586+Scorpio28-Oficial@users.noreply.github.com>
2021-02-04 23:28:24 -0600

    Add files via upload
    Unete a Nuestra Gran Comunidad Informatic in Termux

---
+++ NgrokInstall.sh
@@ -0,0 +1,57 @@
+#!/bin/dash
+# Soy Scorpio28
+atualizar='\033[1;32m'
+echo "$atualizar Actualizando repositórios..."
+apt update && upgrade -y
+apt install -y wget
+clear
+k='\033[01;32m'
+echo
+echo
+echo "Instalar Ngrok? [Y/n]"
+read opcao
+case $opcao in
+y)
+echo
+echo "Descargando Termux-ngrok..."
+case `dpkg --print-architecture` in
+aarch64)
+    architectureURL="arm64" ;;
+arm)
+    architectureURL="arm" ;;
+armhf)
+    architectureURL="armhf" ;;
+amd64)
+    architectureURL="amd64" ;;
+i*86)
+    architectureURL="i386" ;;
+x86_64)
+    architectureURL="amd64" ;;
+*)
+    echo "Arquitectura Desconocida"
+esac
+
+wget "https://github.com/tchelospy/NgrokTest/blob/master/ngrok-stable-linux-${architectureURL}.zip?raw=true" -O ngrok.zip
+unzip ngrok.zip
+cat ngrok > /data/data/com.termux/files/usr/bin/ngrok
+chmod 700 /data/data/com.termux/files/usr/bin/ngrok
+rm ngrok ngrok.zip
+clear
+echo "${k}███╗   ██╗ ██████╗ ██████╗  ██████╗ ██╗  ██╗";
+echo "${k}████╗  ██║██╔════╝ ██╔══██╗██╔═══██╗██║ ██╔╝";
+echo "${k}██╔██╗ ██║██║  ███╗██████╔╝██║   ██║█████╔╝ ";
+echo "${k}██║╚██╗██║██║   ██║██╔══██╗██║   ██║██╔═██╗ ";
+echo "${k}██║ ╚████║╚██████╔╝██║  ██║╚██████╔╝██║  ██╗";
+echo "${k}╚═╝  ╚═══╝ ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝";
+echo
+echo "Ejemplo de uso (ngrok http 80\n)"
+;;
+
+n)
+clear
+echo "Ngrok Instalado :("
+echo
+esac
+
+
+
